bb1e786cf4c86433bbee9f5eb947a5fa22452faf
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-11-19 23:37:02 +0500

    yup removed

--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { User } from '../../interfaces/user.interface';
-import * as Yup from 'yup';
+//import * as Yup from 'yup';
 import http from '../../services/api';
 import { saveToken, setAuthState } from './authSlice';
 import { setUser } from './userSlice';
